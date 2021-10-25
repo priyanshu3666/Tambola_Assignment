411daedd2d340a3c8caf6cff165e2387f37a6028
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-26 02:23:13 +0530

    created random number generator

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,25 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Game extends Thread {
-   
+    static ArrayList<Integer> number_generator_arr = new ArrayList<>(90);
+
+    Game() {
+        int num = 1;
+        while (num != 91) {
+            number_generator_arr.add(num);
+            num = num + 1;
+        }
+    }
+
+    int random_number_generator() {
+        Collections.shuffle(number_generator_arr);
+        System.out.println(number_generator_arr);
+        int removed_element = number_generator_arr.remove(number_generator_arr.size() - 1);
+        System.out.println(removed_element);
+        return removed_element;
+    }
+
+
     public static void main(String[] args) throws InterruptedException {
        
     }
