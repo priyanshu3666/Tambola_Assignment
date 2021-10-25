b7d9a5fb6b4cb1ba84e44775ab3bfe790769bb9d
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-26 02:18:52 +0530

    created ticket generator method

--- src/Tambola.java
+++ src/Tambola.java
@@ -5,5 +5,13 @@ import java.util.Set;
 
 class Tambola {
 
+    HashMap<Integer,Boolean> ticketgenerator() {
+        var ticket = new HashMap<Integer, Boolean>();
+        int min = 1, max = 90;
+        while (ticket.size() != 15) {
+            ticket.put((int) (Math.random() * (max - min + 1) + min), false);
+        }
+        return ticket;
+    }
 
 }
