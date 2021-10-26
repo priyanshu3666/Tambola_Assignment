e20434033c1f2d6cc0d1563492a06ba6b854d008
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-26 11:00:30 +0530

    modify checker method

--- src/Tambola.java
+++ src/Tambola.java
@@ -12,14 +12,12 @@ class Tambola {
 
     String checker(HashMap player, HashSet board) {
         Iterator ticketkey = player.keySet().iterator();
-        int row = 0;
 
         while (ticketkey.hasNext()) {
             Object key = ticketkey.next();
-            if (!(board.contains(key) && (player.get(key) == "true"))) {
+            if (!(board.contains(key) && (boolean)(player.get(key)))) {
                 return "Looser";
             }
-            row++;
         }
         return "Winner";
     }
