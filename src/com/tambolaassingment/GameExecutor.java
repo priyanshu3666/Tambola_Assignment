ff25f20da50759e417e7da37474f8b1e0cbca223
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-30 00:28:11 +0530

    update the code and remove file system operations

--- src/com/tambolaassingment/GameExecutor.java
+++ src/com/tambolaassingment/GameExecutor.java
@@ -1,19 +1,17 @@
 package com.tambolaassingment;
 
-import java.io.File;
+
 
 public class GameExecutor implements  Runnable{
-
     static int randomNumberGenerated=0;
-    final static String filepath = "C:\\Users\\Priyanshu Shukla\\IdeaProjects\\Tambola_Assignment\\src\\RanNoStore.txt";
-
 
     @Override
     public void run() {
+
         while(!Player.housieStatus) {
-            randomNumberGenerated = RandomNumberGenerator.number_generator();
+            randomNumberGenerated = RandomNumberGenerator.numberGenerator();
             try {
-                Thread.sleep(5000);
+                Thread.sleep(498);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -22,21 +20,16 @@ public class GameExecutor implements  Runnable{
 
 
     public static void main(String[] args)  {
-        File f = new File(filepath);
-        if (f.exists()) {
-            f.delete();
-        }
         System.out.println("Let's Begin the Tambola game \n");
-        var board = new TambolaBoard();
+        new TambolaBoard();
         GameExecutor gameExecutor = new GameExecutor();
         Thread gameExecutorThread = new Thread(gameExecutor);
-        Player player1 = new Player("Priyanshu",0);
-        Player player2 = new Player("Yansh",1);
+        Player player1 = new Player("Priyanshu",new TicketChecker());
+        Player player2 = new Player("Yansh",new TicketChecker());
         Thread player1thread = new Thread(player1);
         Thread player2thread = new Thread(player2);
         gameExecutorThread.start();
         player1thread.start();
         player2thread.start();
-        }
-    }
+        }}
 
