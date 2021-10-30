28cba5208776864c00037f4cbb02c49ffa0348a5
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-10-30 12:23:25 +0530

    housie method replaced with counter

--- src/com/tambolaassingment/Player.java
+++ src/com/tambolaassingment/Player.java
@@ -4,14 +4,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 class Player implements Runnable {
-    private TicketChecker ticketChecker;
-    public static  boolean housieStatus = false;
+    private  int totalCount= 0;
+    public static boolean housieStatus = false;
     private final String name;
-    private  HashMap<Integer,Boolean> ticket;
+    private TicketChecker ticketChecker;
+    private HashMap<Integer, Boolean> ticket;
 
     public Player(String name, TicketChecker ticketChecker) {
         this.name = name;
-        this.ticketChecker =ticketChecker;
+        this.ticketChecker = ticketChecker;
 
         ticket = new HashMap<>();
         int min = 1;
@@ -21,18 +22,9 @@ class Player implements Runnable {
         }
 
     }
-    public  HashMap<Integer,Boolean> getTicket(){
-        return  ticket;
-    }
-    boolean housie(HashMap<Integer,Boolean> ticket){
-            for ( Boolean key : ticket.values() ) {
-                if(!(boolean) key) {
-                    return  false;}
-            }
-            return true;
-    }
+    
 
-    void displayticket(HashMap<Integer,Boolean> ticket) {
+    void displayticket(HashMap<Integer, Boolean> ticket) {
         Iterator<Integer> ticketKey = ticket.keySet().iterator();
         int row = 0;
         while (ticketKey.hasNext()) {
@@ -40,35 +32,36 @@ class Player implements Runnable {
                 System.out.println();
             }
             row++;
-            int key =  ticketKey.next();
+            int key = ticketKey.next();
             System.out.print(" " + key + " = " + ticket.get(key) + "      ");
         }
     }
 
-
     void marker() {
+        if (ticket.get(GameExecutor.randomNumberGenerated) == null||!ticket.get(GameExecutor.randomNumberGenerated)){
 
-            var hit = ticket.replace(GameExecutor.randomNumberGenerated, true);
+            Object hit = ticket.replace(GameExecutor.randomNumberGenerated, true);
             if (hit != null) {
+                totalCount++;
+                if(totalCount == 15) {
+                    housieStatus =true;
+                }
                 System.out.println("\n" + name + " got a match in ticket : " + GameExecutor.randomNumberGenerated);
                 displayticket(ticket);
             }
-
-
+        }
     }
 
     public void run() {
 
-        while(!housieStatus) {
+        while (!housieStatus) {
             marker();
-            housieStatus = housie(ticket);
-
             if (housieStatus) {
                 System.out.println("\nHey, its housie ,Please ask checker to check\n ");
                 ticketChecker.tickerChecker(ticket, TambolaBoard.board, name);
             }
             try {
-                Thread.sleep(500);
+                Thread.currentThread().sleep(3002);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
