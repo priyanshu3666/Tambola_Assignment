6541594846f197bc07a7ada0ce3967c4a38e327b
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-11-01 22:13:43 +0530

    while loop replaced with foreach loop

--- src/com/tambolaassingment/TicketChecker.java
+++ src/com/tambolaassingment/TicketChecker.java
@@ -1,22 +1,19 @@
 package com.tambolaassingment;
 
-import java.util.HashMap;
-import java.util.Iterator;
+
 import java.util.Map;
 
 public class TicketChecker {
     void tickerChecker(Map<Integer, Boolean> ticket, Map<Integer, Boolean> board, String playerName) {
         System.out.println("Wait tickerChecker is Checking........ ... .... ... ");
-        Iterator<Integer> ticketkey = ticket.keySet().iterator();
-        while (ticketkey.hasNext()) {
-            Object key = ticketkey.next();
-            if (!(board.get(key).equals(ticket.get(key)))) {
+        for (Map.Entry<Integer, Boolean> pair : ticket.entrySet()) {
+            if (!(board.get(pair.getKey()).equals(ticket.get(pair.getKey())))) {
                 System.out.println("Sorry!! " + playerName + " loose the Game \n  Game Over ");
                 System.exit(0);
             }
+            System.out.println("!!!!Congratulation!!!!\n  " + playerName + "  won the Game");
+            System.exit(0);
         }
-        System.out.println("!!!!Congratulation!!!!\n  " + playerName + "  won the Game");
-        System.exit(0);
     }
-
 }
+
